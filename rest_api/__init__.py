dcb890b49f4fdc42a305c3c0c07c915b981969f4
Christian Schweigel <christian.sw@hotmail.de>
2021-06-15 19:44:40 +0200

    Record time measurement more precise

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -1,4 +1,4 @@
-from bottle import Bottle
+from bottle import Bottle, response
 import os
 HTTP_ORIGIN = os.getenv("HTTP_ORIGIN")
 MAX_STATIONS = int(os.getenv("MAX_STATIONS", 7))
@@ -8,3 +8,14 @@ app = Bottle()
 from .predict import predict_model
 from .error_handler import error404, error400, error403, error500
 
+@app.hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+@app.route("/status")
+def status():
+    return {
+        "status": "OK"
+    }
--- rest_api/predict.py
+++ rest_api/predict.py
@@ -16,6 +16,8 @@ def run_ml_model(model_name, uuids, location):
     model = AVAILABLE_MODELS[model_name]
     mlm = MlManager(model(), scaler())
     e5, e10, diesel = MlManager.get_train_data(uuids)
+    datapulltime = time.time() - starttime
+    starttime = time.time()
     train_data = {
         "e5": e5,
         "e10": e10,
@@ -30,34 +32,38 @@ def run_ml_model(model_name, uuids, location):
         prediction = pipeline.predict(prediction_points)
         prediction = [i[0] for i in prediction]
         predictions[gas_type] = prediction
-    runtime = time.time()-starttime
-    logging.info("Hour prediction called; Model: {}; Running time: {}".format(model_name, runtime))
+    predict_time = time.time()-starttime
+    runtime = predict_time + datapulltime
+    logging.info("Hour prediction called; Model: {}; Running time: {}; Time to predict: {}; Time to pull data: {};".format(model_name, runtime, predict_time, datapulltime))
     return {
         "status": "OK",
         "model_name": model_name,
-        "time_to_predict": runtime,
+        "runtime": runtime,
+        "time_to_predict": predict_time,
+        "time_to_pull_data": datapulltime,
         "hourly_predictions": predictions
     }
 
-@app.post("/predict/<model_name>")
+@app.route("/predict/<model_name>", method=["POST", "OPTIONS"])
 def predict_model(model_name):
     origin = request.headers.get("Origin")
-    if origin != HTTP_ORIGIN and HTTP_ORIGIN != None:
-        abort(403, "Access denied")
-    if model_name not in AVAILABLE_MODELS:
-        abort(404, "Model '{}' does not exist".format(model_name))
-    if request.json == None:
-        abort(400, "Post body must be JSON-encoded")
-    location = request.json["location"]
-    if type(location) is not list or len(location) != 2:
-        abort(400, "Location format wrong")
-    station_uuids = request.json["station_uuids"]
-    if type(station_uuids) is not list:
-        abort(400, "Station UUIDS are not a list")
-    if len(station_uuids) == 0:
-        abort(400, "Station UUIDS are empty")
-    if len(station_uuids) > MAX_STATIONS:
-        station_uuids = station_uuids[:MAX_STATIONS]
-    if len(station_uuids) == 0:
-        abort(400, "Station UUIDS are empty")
-    return run_ml_model(model_name, station_uuids, location)
+    if request.method != "OPTIONS":
+        #if origin != HTTP_ORIGIN and HTTP_ORIGIN != None:
+        #    abort(403, "Access denied")
+        if model_name not in AVAILABLE_MODELS:
+            abort(404, "Model '{}' does not exist".format(model_name))
+        if request.json == None:
+            abort(400, "Post body must be JSON-encoded")
+        location = request.json["location"]
+        if type(location) is not list or len(location) != 2:
+            abort(400, "Location format wrong")
+        station_uuids = request.json["station_uuids"]
+        if type(station_uuids) is not list:
+            abort(400, "Station UUIDS are not a list")
+        if len(station_uuids) == 0:
+            abort(400, "Station UUIDS are empty")
+        if len(station_uuids) > MAX_STATIONS:
+            station_uuids = station_uuids[:MAX_STATIONS]
+        if len(station_uuids) == 0:
+            abort(400, "Station UUIDS are empty")
+        return run_ml_model(model_name, station_uuids, location)
